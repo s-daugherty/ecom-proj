6621d77628ff4ca66acc7ba06ccef231084007f7
samanthadaugherty <samdaugherty26@gmail.com>
2021-01-27 22:09:56 -0500

    Basic categories

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
