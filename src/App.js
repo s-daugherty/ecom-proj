cab52608ec2f2238b9f5a313a157eefe4ea30f6b
samanthadaugherty <samdaugherty26@gmail.com>
2021-01-28 00:13:30 -0500

    adding react router dom for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -7,7 +8,9 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
